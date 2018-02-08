fae7d0e9c3e4a2bdfc93f4455db95a589346fc3b
Vincent Klein <vincent.klein@u-bordeaux.fr>
2018-02-08 10:14:26 +0100

    Travis: sphinx add include path to conf.py again

--- sphinx-doc/source/conf.py
+++ sphinx-doc/source/conf.py
@@ -15,7 +15,9 @@
 import sys
 import os
 
-sys.path.append('../../..')
+# Add all the documented module to sys.path
+sys.path.append('../..')
+sys.path.insert(0, os.path.abspath('../..'))
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
